bebc36418aef6540bb37228febaa4f251df73ddc
Jovane Maluto <jovane.maluto@Liralink.intra>
2024-08-19 09:17:08 +0100

    :tada: initial commit

---
+++ script.js
@@ -0,0 +1,25 @@
+function toggleMode() {
+  const html = document.documentElement
+  html.classList.toggle("light")
+
+  /*
+  ESTE CÓDIGO PODE SER SUBSTITUÍDA PELA LINHA DE CÓDIGO ACIMA: "html.classList.toggle("light")" 
+  if (html.classList.contains("light")) {
+    html.classList.remove("light")
+  } else {
+    html.classList.add("light")
+  }
+  */
+
+  //PEGAR A TAG IMG
+  const img = document.querySelector("#profile img")
+
+  //SUBSTITUIR A IMAGEM
+  if (html.classList.contains("light")) {
+    //se tiver light mode, adicionar a imagem light
+    img.setAttribute("src", "./assets/avatar-light.png")
+  } else {
+    //se tiver sem light mode, manter a imagem normal
+    img.setAttribute("src", "./assets/avatar.png")
+  }
+}
